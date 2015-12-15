b991f7e2f00000141c5b78f07fc0a98067f37d1f
Aimin Yan <jlfmssm@gmail.com>
2015-12-15 11:34:35 -0500

    Update RunAlignment4PairEnd.sh

--- RNA-seq-data-analysis-pipeline/RunAlignment4PairEnd.sh
+++ RNA-seq-data-analysis-pipeline/RunAlignment4PairEnd.sh
@@ -1,23 +1,19 @@
 #! /bin/bash
 
-#sh ~/Script_bash/RunAlignment4SingleEnd.sh Sample_9.txt  
+#sh ~/Script_bash/RunAlignment4PairEnd.sh Sample_9.txt  
 while read line; do
 
-#f1=`echo "$line" | awk -F"\t" '{print $1}'`
-#f2=`echo "$line" | awk -F"\t" '{print $2}'`
+f1=`echo "$line" | awk -F"\t" '{print $1}'`
+f2=`echo "$line" | awk -F"\t" '{print $2}'`
 
-f=`echo "$line"`
-
-echo "$f"
-sample_name=`echo "$f" | awk -F"." '{print $1}'`
+echo "$f1\t$f2"
+sample_name=`echo "$f1" | awk -F"_" '{print $3}'`
 
 echo "$sample_name"
 
-cat > ~/Script_bash/Run_"$sample_name"_tophat.sh <<EOF
-tophat -G ~/genes.gtf -p 4 -o "$sample_name"_tophat_out ~/mm10_index_bt2/genome "$f"
+cat > Run_"$sample_name"_tophat.sh <<EOF
+tophat -G genes.gtf -p 4 -o "$sample_name"_tophat_out mm10_index_bt2/genome ~/RNAseqData/"$f1" ~/RNAseqData/"$f2"
 mv "$sample_name"_tophat_out/accepted_hits.bam "$sample_name".bam
 EOF
-
-bsub -P Bioinformatics4count < ~/Script_bash/Run_"$sample_name"_tophat.sh
 
 done < $1
